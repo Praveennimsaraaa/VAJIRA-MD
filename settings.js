71b7dd1593fda48f101125fe6b782bf4f931a73c
Praveennimsaraaa <Sampathvishva07@gmail.com>
2024-05-06 16:21:12 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -24,7 +24,7 @@ global.mess = {
 
 global.owner = [process.env.OWNER_NUMBER] || ['94761017479'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
 
-module.exports = {    
+module.exports = {782891894    
 SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD:CoursingConnie',
 POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'postgres://izumimd_meje_user:0Vhm5vKGZ7ORt2FlJBQf4d6EtRdeuE8z@dpg-cn0o2imn7f5s73fa46q0-a.frankfurt-postgres.render.com/izumimd_meje',
 PREFIX:  process.env.PREFIX || ['.'] ,
